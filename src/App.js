ed69428eb11a2b13016ba86e23fd2aa1061ada10
Nanthasen Lam <bunkylam@Nanthasens-MacBook-Air.local>
2023-04-14 15:33:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './App.css'
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  //save your count using local storage, getitem first, then setitem
+  useEffect(() => {
+    setCount(JSON.parse(window.localStorage.getItem('count')))
+  }, []);
+
+
+  useEffect(() => {
+    window.localStorage.setItem('count', count)
+  }, [count]);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>You clicked {count} times</p>
+      <div className="clickButton">
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+        <div className="clearButton">
+          <button onClick={() => setCount(0)}>
+            Click to clear
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
